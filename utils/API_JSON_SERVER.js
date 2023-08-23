fa8e54178f95e2a8052116a7496052dda32f4cd5
immo9769 <mullaimran057@gmail.com>
2023-08-23 11:43:24 +0530

    changes by Imran

--- utils/API_JSON_SERVER.js
+++ utils/API_JSON_SERVER.js
@@ -347,3 +347,4 @@ const postOTP = async (data) => {
 
 // export {getCustomers, getProducts, getServices, getDeliveryTypes,getEmirates,getArea, postOrder,getOrders, postSignUp,putProfile,postOTP};
 // changes by Ikram 
+// changes by Imran
